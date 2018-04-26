d80a302b98cefebb6a0946d51098584a1b0d6a09
AFakeman <anton_suslov@me.com>
2018-04-26 14:37:35 +0300

    wip

--- gunicorn/api/models.py
+++ gunicorn/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+import uuid
+
+
+class Event(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
+    users = models.ManyToManyField(User)
+    uuid = models.UUIDField(primary_key=True,
+                            default=uuid.uuid4, editable=False)
--- gunicorn/run.sh
+++ gunicorn/run.sh
@@ -3,4 +3,5 @@
 python manage.py collectstatic --noinput && 
 python manage.py makemigrations && 
 python manage.py migrate && 
-daphne -b 0.0.0.0 -p 8000 backend.asgi:application
+daphne -b 0.0.0.0 -p 8000 backend.asgi:application ||
+echo ERROR
