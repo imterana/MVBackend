411a1cb8de29ea3609003b23b76d0c2c3e0406cc
terana <terenteva8412@gmail.com>
2018-05-07 15:31:13 +0300

    BACK_89: Added HTTP method to view

--- gunicorn/api/auth/views.py
+++ gunicorn/api/auth/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_GET
 
 from ..misc.response import (
     APIResponse,
 )
 
 
+@require_GET
 @login_required
 def get_current_user_id(request):
     user = request.user
